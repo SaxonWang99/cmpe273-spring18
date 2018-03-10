561010a0869e7980454430169c07bcc1b9967a62
SaxonWang99 <shihan.wang@sjsu.edu>
2018-03-09 23:17:59 -0800

    Second Commit

--- quizzes/quizz2/hello.py
+++ quizzes/quizz2/hello.py
@@ -1,13 +1,31 @@
-from flask import Flask
+from flask import Flask, jsonify, abort
 from flask import request
-
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+users=[]
+
+@app.route("/")
 def hello():
     return "Hello World!"
 
 @app.route('/users', methods=['POST'])
-def new_users():
-    name = request.form["name"]
-    return "Hello {}!".format(name)
+def addUser():
+    name = request.form['name']
+    user = {'id': len(users)+1, 'name': name}
+    users.append(user)
+    return jsonify(user)
+
+@app.route('/users/<int:userId>', methods=['GET'])
+def getUser(userId):
+    if len(users) == 0 or userId > len(users):
+        abort(404)
+    user = users[0]
+    return jsonify(user)
+
+@app.route('/users/<int:userId>', methods=['DELETE'])
+def deleteUser(userId):
+    if len(users) == 0 or userId > len(users):
+        abort(404)
+    user = users[userId-1]
+    users.remove(user)
+    return jsonify({'result': True})
